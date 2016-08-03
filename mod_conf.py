efb45965f8919c79aac3c2729237cab7b34d3751
Slashdoom <slashdoom@users.noreply.github.com>
2016-08-03 22:24:27 +1200

    Update mod_conf.py

--- mod_conf.py
+++ mod_conf.py
@@ -1,15 +1,30 @@
 # Set location of CDR files
-# Example: cdr_path = "/opt/ftproot/"
-cdr_path = "/mnt/sdb1/ftproot/"
+# Example: cdr_path = "/opt/cdr_ftproot/"
+cdr_path = "/opt/cdr_ftproot/"
 
 # Set location where CDR files are archived to after processing
-# Example: archive_path = "/tmp/cdr/archive/"
-archive_path = "/mnt/sdb1/archive/"
+# Example: archive_path = "/opt/cdr_archive/"
+archive_path = "/opt/cdr_archive/"
+
+# Set checking for files already existing in ElasticSearch
+# Warning: Experimental
+# Check for duplicates and don't write to ES:
+# es_file_check = True
+# Don't check for duplicates and write duplicate files to ES:
+# es_file_check = False
+es_file_check = False
+
+# Set ElasticSearch Host
+# Exmaple: es_host = "192.168.1.100"
+es_host = "localhost"
+
+# Set ElasticSearch Port
+# Example: es_port = "9200"
+es_port = "9200"
 
 # Set ElasticSearch Index
+# Default normally recommended.  Uses cdr-[date] and cmr-[date]
 # Example: es_index = "cdr"
-# from datetime import datetime
-# es_index = "cdr-%s" % datetime.utcnow().strftime("%Y.%m.%d")
 es_index = ""
 
 # Set ElasticSearch Type
